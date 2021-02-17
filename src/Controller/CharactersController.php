ab51e240b276404d1d16de9e064c418c9d53d4a3
Rui Leite <ruihloliveira@gmail.com>
2021-02-17 11:12:08 -0300

    character creation

--- src/Controller/CharactersController.php
+++ src/Controller/CharactersController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use Exception;
+use Throwable;
+use App\Entity\Game;
 use App\Entity\Character;
+use App\Exception\ValidationException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CharactersController extends AbstractController
 {
@@ -15,12 +19,34 @@ class CharactersController extends AbstractController
     /**
      * @Route("/characters", methods={"GET","HEAD"})
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $characters = $this->getDoctrine()
-        ->getRepository(Character::class)
-        ->findAll();
-        return new JsonResponse(compact('characters'));
+        try {
+            $filters = $request->query->get('filters');
+    
+            if($filters !== null) {
+                $filters = json_decode($filters,true);
+                foreach ($filters as $key => $value) {
+                    $filters[key($value)] = $value[key($value)];
+                    unset($filters[$key]);
+                }
+            }
+    
+            $filters['user'] = $this->getUser();
+            
+            $repository = $this->getDoctrine()->getRepository(Character::class);
+            $characters = null;
+    
+            if($filters == null){
+                $characters = $repository->findBy($filters);
+            } else {
+                $characters = $repository->findBy($filters);
+            }
+            
+            return new JsonResponse(compact('characters'));
+        } catch (\Exception $e) {
+            return new JsonResponse(['message' => $e->getMessage()],500);
+        }
     }
 
     /**
@@ -28,16 +54,42 @@ class CharactersController extends AbstractController
      */
     public function store(Request $request): Response
     {
-        $requestData = $request->toArray();
-        $characters = new Character();
-        $characters->setName($requestData['name']);
-        $characters->setHp(100);
-        
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($characters);
-        $em->flush();
-        $characters->getId();
-        
-        return new JsonResponse(compact('characters'));
+        try {
+            
+            // if($project == null) {
+            //     $this->createNotFoundException("Project Not Found");
+            // }
+            $requestData = $request->toArray();
+            if(!isset($requestData['name']) || $requestData['name'] == null || $requestData['name'] == '') {
+                throw new ValidationException('Cant create a Character without "Name".');
+            }
+            if(!isset($requestData['game']) || $requestData['game'] == null || $requestData['game'] == '') {
+                throw new ValidationException('Cant create a Character without "Game".');
+            }
+
+            $game = $this->getDoctrine()->getRepository(Game::class)->findOneBy([
+                'id' => $requestData['game']
+            ]);
+            if($game == null) {
+                $this->createNotFoundException("Game Not Found");
+            }
+
+            $character = new Character();
+            $character->setName($requestData['name']);
+            $character->setGame($game);
+            $character->setUser($this->getUser());
+            $character->setHp(100);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($character);
+            $em->flush();
+            $character->getId();
+            return new JsonResponse(compact('character'));
+        } catch (ValidationException $e) {
+            return new JsonResponse(['message' => $e->getMessage()],500);
+        } catch (Throwable $e) {
+            throw $e;
+            return new JsonResponse(['message' => 'An error ocurred while creating a character.'],500);
+        }
     }
 }
